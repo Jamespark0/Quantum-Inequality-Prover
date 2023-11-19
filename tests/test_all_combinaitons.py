acb676cc13be7fc5282d3c547524a4000ba6d217
Shao-Lun Huang <simon.linux7@gmail.com>
2023-11-19 10:41:36 +0100

    Change tuple of sets to that of frozensets. But this file is to be removed

--- tests/test_all_combinaitons.py
+++ tests/test_all_combinaitons.py
@@ -2,21 +2,21 @@ from classical import g_creator
 
 
 def test_from_no_element() -> None:
-    assert g_creator.all_combinations(n=0) == ()
+    assert g_creator.all_pairs(n=0) == ()
 
 def test_from_single_element() -> None:
-    assert g_creator.all_combinations(n=1) == ({1},)
+    assert g_creator.all_pairs(n=1) == ({1},)
 
 def test_from_two_element() -> None:
-    assert g_creator.all_combinations(n=2) == ({1}, {2}, {1,2},)
+    assert g_creator.all_pairs(n=2) == ({1}, {2}, {1,2},)
     
 def test_from_three_element() -> None:
-    assert g_creator.all_combinations(n=3) == ({1},{2},{3},{1,2},{1,3},{2,3},{1,2,3},)
+    assert g_creator.all_pairs(n=3) == ({1},{2},{3},{1,2},{1,3},{2,3},{1,2,3},)
 
 def test_from_four_element() -> None:
-    all_pairings: tuple[set[int], ...] = g_creator.all_combinations(n = 4)
+    all_pairings = g_creator.all_pairs(n = 4)
     assert len(all_pairings) == 2**4 - 1
 
 def test_from_ten_element() -> None:
-    all_pairings = g_creator.all_combinations(n = 10)
+    all_pairings = g_creator.all_pairs(n = 10)
     assert len(all_pairings) == 2**10 - 1
