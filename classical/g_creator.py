b1b75e5dff3297a1dbf3c02e3b9603549c2dbae8
Shao-Lun Huang <simon.linux7@gmail.com>
2023-11-20 14:32:02 +0100

    On branch developing
    Add a helper method in ShannonInequality to help generating the first
    type of elemetnal inequalities.
    
    Changes to be committed:
    modified:   classical/g_creator.py
    modified:   tests/test_shannon_inequality.py

--- classical/g_creator.py
+++ classical/g_creator.py
@@ -9,6 +9,8 @@ class ShannonInequality:
     def __post_init__(self):
         self.all_pairs: tuple = self._get_all_pairs(self.n)
         self.to_index = dict(zip(self.all_pairs, [x for x in range(2**self.n - 1)]))
+        # to be deleted, just for testing
+        self.index_to_pair = dict(zip([x for x in range(2**(self.n) - 1)], self.all_pairs))
 
     # Just to help generate pairs
     def _get_all_pairs(self, n: int) -> tuple[frozenset[int], ...]:
@@ -21,8 +23,26 @@ class ShannonInequality:
                 for x in combinations(random_variable_labeling, r=_)
             ]
         )
+    
+    def _get_type1_elemental_entropic_vector(self, i: int) -> list[int]:
+        """
+        The type 1 elemental form is the conditional entropy, taking the form of
+        H(X_{i}|X_{N-{i}})
 
+        Args:
+            i (int): The random variable will focus on
+
+        Returns:
+            list[int]: The entropic vector of the given conditional entropy
+        """
+        elemental_vector_i: list[int] = [0 for _ in range(2**(self.n) - 1)]
+        elemental_vector_i[-1] = 1
+        elemental_vector_i[self.to_index[self.all_pairs[-1] - {i}]] = -1
+
+        return elemental_vector_i
 
 if __name__ == "__main__":
     shannon_inequality = ShannonInequality(n=3)
-    print(shannon_inequality.__dict__)
+    print(shannon_inequality.all_pairs)
+    print(shannon_inequality.all_pairs[-1] - {1})
+    print(shannon_inequality._get_type1_elemental_entropic_vector(1))
--- tests/test_shannon_inequality.py
+++ tests/test_shannon_inequality.py
@@ -6,26 +6,52 @@ def test_from_no_random_variable() -> None:
 
     assert shannon.all_pairs == ()
 
+
 def test_from_single_element() -> None:
     shannon = ShannonInequality(n=1)
 
     assert shannon.all_pairs == ({1},)
 
+
 def test_from_two_element() -> None:
     shannon = ShannonInequality(n=2)
 
-    assert shannon.all_pairs == ({1}, {2}, {1,2},)
-    
+    assert shannon.all_pairs == (
+        {1},
+        {2},
+        {1, 2},
+    )
+
+
 def test_from_three_element() -> None:
     shannon = ShannonInequality(n=3)
 
-    assert shannon.all_pairs == ({1},{2},{3},{1,2},{1,3},{2,3},{1,2,3},)
+    assert shannon.all_pairs == (
+        {1},
+        {2},
+        {3},
+        {1, 2},
+        {1, 3},
+        {2, 3},
+        {1, 2, 3},
+    )
+
 
 def test_from_four_element() -> None:
     shannon = ShannonInequality(n=4)
 
     assert len(shannon.all_pairs) == 2**4 - 1
 
+
 def test_from_ten_element() -> None:
-    shannon = ShannonInequality(n = 10)
+    shannon = ShannonInequality(n=10)
     assert len(shannon.all_pairs) == 2**10 - 1
+
+
+def test_type1_elemental_inequalities() -> None:
+    shannon = ShannonInequality(n=10)
+    for i in range(10):
+        conditional_entropic_vector = shannon._get_type1_elemental_entropic_vector(i=i)
+        assert shannon.index_to_pair[
+            conditional_entropic_vector.index(-1)
+        ] == shannon.all_pairs[-1] - {i}
