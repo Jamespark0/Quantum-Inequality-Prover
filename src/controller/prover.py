d428ab88869dcb5ddc983dd5a7c43433d9c3166e
Shao-Lun Huang <simon.linux7@gmail.com>
2024-01-11 15:47:56 +0100

    On branch developing
    Complete von-Neumann entropy proof generator, and not provable hints
    generator.
    
    Changes to be committed:
    modified:   src/controller/prover.py
    modified:   src/mini_app.py
    modified:   src/view/view_prover.py

--- src/controller/prover.py
+++ src/controller/prover.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from xml.dom.minidom import Element
 
 import numpy as np
 from numpy.typing import NDArray
@@ -85,3 +86,83 @@ class Prover:
             if result.success
             else (None, None)
         )
+
+    # Different from classical information theory
+    # due to conditional entropy can be negative in the quantum case
+    def disprove_dual_gamma(
+        self, n: int, inequality: NDArray, constraints: NDArray
+    ) -> NDArray:
+        # Recall that quantum conditional entropy can be negative -> S(universal) is not the maximum entropy
+        # The minimum value should be positive, as linprog finds minimum value
+        result = linprog(
+            c=np.array(
+                [
+                    0 if i < (self.elemental.shape[0] + constraints.shape[0]) else 1
+                    for i in range(self.elemental.shape[0] + constraints.shape[0] + n)
+                ]
+            ),
+            A_eq=np.vstack(
+                (
+                    np.vstack((self.elemental, -constraints)),
+                    -1 * np.eye(N=n, M=inequality.shape[1], dtype=np.int64),
+                )
+            ).transpose(),
+            b_eq=inequality,
+            bounds=tuple(
+                [(0, None)] * self.elemental.shape[0]
+                + [(None, None)] * constraints.shape[0]
+                + [(0, None)] * n
+            ),
+            method="interior-point",
+            options={"disp": False},
+        )
+
+        if result.success is False:
+            raise ValueError("Unable to find the optimal solution!")
+
+        # print(f"Success: {result.success}")
+        # print(
+        #     f"The minimum value for non-von-Neumann type, {-1 * result.fun if result.success else None}, should be negative"
+        # )
+
+        return result.x[-n:]
+
+    def disprove_primal_gamma(self, inequality: NDArray, constraints: NDArray):
+        result = linprog(
+            c=inequality[0],
+            A_ub=-self.elemental,
+            b_ub=np.zeros(self.elemental.shape[0]),
+            A_eq=constraints,
+            b_eq=np.zeros(constraints.shape[0]),
+            bounds=tuple([(0, 10)] * inequality.shape[1]),
+            method="highs",
+        )
+
+        if result.x is None:
+            raise ValueError("No optimal solution!")
+
+        print(f"Success: {result.success}")
+        print(
+            f"The minimum value for non-von-Neumann type, {result.fun if result.success else None}, should be negative"
+        )
+        print(result.ineqlin.marginals)
+
+        return -result.fun if result.success else np.inf
+
+    def shortest_disprove_generator(
+        self, n: int, inequality: NDArray, constraints: NDArray
+    ) -> tuple[NDArray, NDArray] | tuple[None, None]:
+        gamma: NDArray = self.disprove_dual_gamma(
+            n=n, inequality=inequality, constraints=constraints
+        )
+
+        return self.shortest_proof_generator(
+            inequality=inequality
+            + np.matmul(gamma, np.eye(N=n, M=inequality.shape[1])),
+            constraints=constraints,
+        )
+
+    def _universal_set_basis_vector(self, dim: int) -> NDArray:
+        basis = np.zeros(dim)
+        basis[-1] = 1
+        return basis
